6889b1491b633a50592fd645a741b8552dadac8a
Arifin <bustanul.bmi@gmail.com>
2026-05-16 11:10:04 +0700

    feat: Add missing Section component imports in AccessItemForm, CategoryForm, LinkForm, and UserForm

--- app/Filament/Resources/AccessItems/Schemas/AccessItemForm.php
+++ app/Filament/Resources/AccessItems/Schemas/AccessItemForm.php
@@ -3,10 +3,10 @@
 namespace App\Filament\Resources\AccessItems\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Database\Eloquent\Builder;
 
